f7346cbe8fc6e6f0ac27a7e03e5231ae71f3732e
Saurabh Rai <raisaurabh625@gmail.com>
2025-01-17 14:40:45 +0530

    divsion proogram

---
+++ src/division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class division
+    {
+        public static void main (String[] args)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the number of sides: ");
+        }
+}
