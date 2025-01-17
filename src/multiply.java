b6b2fc36c910b20f9d7443add2224f9130aa5aaa
Saurabh Rai <raisaurabh625@gmail.com>
2025-01-17 14:37:50 +0530

    product

---
+++ src/multiply.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class multiply {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+    }
+}
