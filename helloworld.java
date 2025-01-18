bb668cb68d8acd643bc751470250cb2bdfcebeaa
Saurabh Rai <raisaurabh625@gmail.com>
2025-01-18 14:11:51 +0530

    ho gya ji ho gya

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class helloworld
+{
+public static void main (String[] args)
+{
+int i=0;
+while (i<=10)
+{
+if(i%2==0)
+System.out.println("even"+i);
+else
+System.out.println("odd"+i);
+i++;
+}
+}
+}
